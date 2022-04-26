12a44236a14d224948eae73729a1127a167b2956
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-04-26 17:15:12 -0500

    Primer commit

---
+++ Ninja.js
@@ -0,0 +1,25 @@
+class Ninja{
+
+    constructor(nombre, salud){
+        this.nombre = nombre;
+        this.salud = salud || 10;
+        this.velocidad = 3;
+        this.fuerza = 3;
+    }
+
+    sayName(){
+        console.log(this.nombre)
+    }
+
+    showStats(){
+        console.log(`Nombre: ${this.nombre}, Fuerza: ${this.fuerza}, Velocidad: ${this.velocidad}, Salud: ${this.salud}`);
+    }
+
+    drinkSake(){
+        this.salud =+ 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
